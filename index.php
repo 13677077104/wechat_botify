b65273f3b3d93029288057a8229666b5fdd1efbc
Zane <30644457+13677077104@users.noreply.github.com>
2024-01-25 11:06:28 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,82 @@
+<?php
+
+include_once "Utils.php";
+
+
+class wechat {
+    protected $client;
+    protected $appId;
+    protected $secret;
+
+    /**
+     * @throws Exception
+     */
+    public function __construct()
+    {
+        $baseUrl = 'https://api.weixin.qq.com';
+        $this->client = new Utils();
+        $this->client->setBaseUrl($baseUrl);
+        $this->appId = getenv('app_id');
+        $this->secret = getenv('app_secret');
+        if (!$this->appId) {
+            throw new Exception('appid 获取失败');
+        }
+        if (!$this->secret) {
+            throw new Exception('appid 获取失败');
+        }
+    }
+
+    /**
+     * @description 获取access_token
+     * @return string
+     * @throws Exception
+     */
+    protected function getAccessToken(): string
+    {
+        $response = $this->client->get('cgi-bin/token', [
+            'grant_type' => 'client_credential',
+            'appid' => $this->appId,
+            'secret' => $this->secret,
+        ]);
+        if (isset($response['error'])) {
+            throw new Exception($response['error']);
+        }
+        if (isset($response['errcode'])) {
+            throw new Exception($response['errmsg']);
+        }
+        return $response['access_token'];
+    }
+
+    /**
+     * @description 发送消息通知
+     * @return void
+     * @throws Exception
+     */
+    public function sendTemplate()
+    {
+        try {
+            $accessToken = $this->getAccessToken();
+            $openId = 'oi4Me6y3rYKj330yUgKU0M4lQp-4';
+            $this->client->post('cgi-bin/message/template/send', [
+                'touser' => $openId,
+                'template_id' => 'HrrZc24lTvxo5ElbQ5PaOFjB9FZ3J8qD-wpYYIaW6Gc',
+                'data' => [
+                    'today' => [
+                        'value' => date('Y-m-d'),
+                    ],
+                    'weather' => [
+                        'value' => '天气好冷',
+                    ],
+                ],
+            ], [
+                'access_token' => $accessToken,
+            ]);
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+    }
+}
+
+(new wechat())->sendTemplate();
+
+echo "success";
